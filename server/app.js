47bd26df7549969ef6ca58cc7d6a1422cd978891
Bakar <ironsamurai786@gmail.com>
2024-03-13 09:44:18 +0500

    added cors origin v2

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://crud-api-fawn.vercel.app/",
+    origin: "https://crud-api-fawn.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
